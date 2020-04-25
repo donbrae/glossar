8522e73b3578f0a12978238d25933ddb3f82e355
Jamie Smith <jamie@donbrae.co.uk>
2020-04-25 21:45:32 +0100

    Build script now adds timestamp automatically.

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 shopt -s extglob
+
+timestamp() {
+  TS=$(date +%s)
+}
+
 cat ./app.js ./data/glossar.js ./data/glossar-v.js ./data/glossar-adj.js ./data/glossar-adv.js ./data/glossar-n.js ./data/glossar-geo.js ./data/glossar-phr.js > glossar-bundle.min.js
+
+timestamp
+
+sed "s/TIMESTAMP/$TS/" index.dev.html > index.html
+
 shopt -u extglob
